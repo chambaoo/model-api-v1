2adc5839feb936dd013a6d197dd508cc2830709f
chambaoo <chambao752@gmail.com>
2026-06-13 12:00:20 +0000

    add: initial commit

---
+++ app/src/main/java/com/example/api/v1/Apiv1Application.java
@@ -0,0 +1,14 @@
+package com.example.api.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apiv1Application {
+
+	public static void main(String[] args) {
+		System.out.println("🐟");
+		SpringApplication.run(Apiv1Application.class, args);
+	}
+
+}
