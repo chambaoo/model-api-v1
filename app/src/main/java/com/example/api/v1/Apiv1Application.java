684535f50a39795775801029108f6d7a5e810cdd
chambaoo <chambao752@gmail.com>
2026-06-13 12:33:22 +0000

    add: spring web

--- app/src/main/java/com/example/api/v1/Apiv1Application.java
+++ app/src/main/java/com/example/api/v1/Apiv1Application.java
@@ -2,13 +2,19 @@ package com.example.api.v1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Apiv1Application {
 
 	public static void main(String[] args) {
-		System.out.println("🐟");
 		SpringApplication.run(Apiv1Application.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World!";
+	}
 }
