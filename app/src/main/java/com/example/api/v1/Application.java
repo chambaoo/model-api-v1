3ed5f5968f5e46ad572e8dec5396a47168137fd5
chambaoo <chambao752@gmail.com>
2026-06-16 10:48:35 +0000

    update: rename application class

--- app/src/main/java/com/example/api/v1/Application.java
+++ app/src/main/java/com/example/api/v1/Application.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Apiv1Application {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Apiv1Application.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }
