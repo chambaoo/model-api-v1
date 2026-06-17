9e121ecf6ee86fc7e37cee7f2214f2a2760e4fc8
chambaoo <chambao752@gmail.com>
2026-06-17 14:44:34 +0000

    update: add entity constructors

--- app/src/main/java/com/example/api/v1/domain/entity/Note.java
+++ app/src/main/java/com/example/api/v1/domain/entity/Note.java
@@ -3,8 +3,12 @@ package com.example.api.v1.domain.entity;
 import com.example.api.v1.domain.object.NoteContent;
 import com.example.api.v1.domain.object.NoteId;
 import java.time.ZonedDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Note {
   private NoteId id;
